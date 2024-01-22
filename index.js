f61e75bfdbd01201e4c2ced7bbdb884dd094d183
silas <silasgebhart12@gmail.com>
2024-01-22 10:31:45 -0600

    employee tracker

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+let employee = {
+  name: "Silas",
+  streetAddress: "123 Mainstreet St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function doesNotModifyOriginalEmployee(employee, key) {
+  return !(key in employee);
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
